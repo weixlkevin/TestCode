6b3c7a49a6e6a2336398a96c871e37a40c38265a
weixlkevin <weixl.kevin@gmail.com>
2014-03-31 17:49:49 +0800

    修改ProxyPattern,进行扩展需要.

---
+++ ProxyPattern/GamePlayerProxy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class GamePlayerProxy : IGamePlayer, IProxy
+    {
+        private IGamePlayer gamePlayer = null;
+
+        public GamePlayerProxy(string name)
+        {
+            this.gamePlayer = new GamePlayer(name);
+        }
+
+        public void Login(string user, string password)
+        {
+            this.gamePlayer.Login(user, password);
+        }
+
+        public void KillBose()
+        {
+            this.gamePlayer.KillBose();
+        }
+
+        public void Upgrade()
+        {
+            this.gamePlayer.Upgrade();
+        }
+
+        public void Count()
+        {
+            Console.WriteLine("升级总费用是: 150元.");
+        }
+    }
+}
--- ProxyPattern/Program.cs
+++ ProxyPattern/Program.cs
@@ -11,12 +11,19 @@ namespace ProxyPattern
         static void Main(string[] args)
         {
             IGamePlayer player = new GamePlayer("张三");
-            Console.WriteLine("游戏开始时间:"+DateTime.Now);
-            player.Login("zhangsan", "password");
-            player.KillBose();
-            player.Upgrade();
+            //Console.WriteLine("游戏开始时间:"+DateTime.Now);
+            //player.Login("zhangsan", "password");
+            //player.KillBose();
+            //player.Upgrade();
+            //Console.WriteLine("游戏结束时间:" + DateTime.Now);
+            GamePlayerProxy proxy = new GamePlayerProxy("张三");
+            Console.WriteLine("游戏开始时间:" + DateTime.Now);
+            proxy.Login("zhangsan", "password");
+            proxy.KillBose();
+            proxy.Upgrade();
+            proxy.Count();
             Console.WriteLine("游戏结束时间:" + DateTime.Now);
-            
+
             Console.ReadKey();
         }
     }
